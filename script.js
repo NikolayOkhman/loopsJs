241d1b8d2b2b87f643c3b252930168b91a6f5207
NikolayOkhman <114661898+NikolayOkhman@users.noreply.github.com>
2022-11-09 11:19:52 +0200

    Add files via upload
    לולאות JS

---
+++ script.js
@@ -0,0 +1,15 @@
+let array = [];
+array[0] = prompt("Enter 1st word");
+array[1] = prompt("Enter 2nd word");
+array[2] = prompt("Enter 3rd word");
+array[3] = prompt("Enter 4th word");
+array[4] = prompt("Enter 5th word");
+array[5] = prompt("Enter 6th word");
+array[6] = prompt("Enter 7th word");
+array[7] = prompt("Enter 8th word");
+array[8] = prompt("Enter 9th word");
+array[9] = prompt("Enter 10th word");
+function displayArray(x) {
+   console.log("The word is " + x);
+}
+array.forEach(displayArray);
